c8a20bc986291546a6a04eba0aa1eca97473cb28
Ryan Murphy <TheDecoder007@gmail.com>
2022-05-10 13:06:49 -0400

    generating one random letter

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"     
+var lowerLetters =  "abcdefghijklmnopqrstuvwxyz"  
+var passNumbers = []
 
 // Write password to the #password input
 function writePassword() {
@@ -13,23 +16,35 @@ function writePassword() {
 
 }
 
-var generatePassword = function() {
-  var passLength = window.prompt("Enter the length of your password between 8-128 characters.");
+generatePassword = function() {
+   var passLength =  
+     window.prompt("Enter the length of your password between 8-128 characters.");
     if (passLength < 8 || passLength > 128) {
       window.alert("Please pick a number between 8 - 128");
       return generatePassword();
     }
-  if (passLength >= 8 || passLength <= 128) {
-      var upperLetters = window.confirm("Would you like upper case letters?");
-      if (upperLetters) {
-        var lowerLetters = window.confirm("Would you like lower case letters?");
-        if (lowerLetters) {
-          var passNumbers = window.confirm("Would you like numbers?");
-        }
+  
+    else  { 
+      
+      var chooseCaps = window.confirm("Would you like upper case letters?");
+    
+      if(chooseCaps) {
+         return upperLetters[Math.floor(Math.random() * upperLetters.length)]  
+      }
+      else {
+      var chooseLow = window.confirm("Would you like lower case letters?");
+
+      if (chooseLow) {
+        return lowerLetters[Math.floor(Math.random() * lowerLetters.length)]
       }
     }
+    }
+  
 };
 
+// passNumbers = window.confirm("Would you like numbers?");       
+// lowerLetters = window.confirm("Would you like lower case letters?");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
