b56d830ca51a0b18f0d55bb362769f2836ade3e1
Ryan Murphy <TheDecoder007@gmail.com>
2022-05-10 11:12:46 -0400

    set up window prompts

--- script.js
+++ script.js
@@ -13,5 +13,23 @@ function writePassword() {
 
 }
 
+var generatePassword = function() {
+  var passLength = window.prompt("Enter the length of your password between 8-128 characters.");
+    if (passLength < 8 || passLength > 128) {
+      window.alert("Please pick a number between 8 - 128");
+      return generatePassword();
+    }
+  if (passLength >= 8 || passLength <= 128) {
+      var upperLetters = window.confirm("Would you like upper case letters?");
+      if (upperLetters) {
+        var lowerLetters = window.confirm("Would you like lower case letters?");
+        if (lowerLetters) {
+          var passNumbers = window.confirm("Would you like numbers?");
+        }
+      }
+    }
+};
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
