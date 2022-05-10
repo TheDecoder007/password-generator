0055315f8d581e983deca6273566400fbf686c76
Ryan Murphy <TheDecoder007@gmail.com>
2022-05-10 13:44:45 -0400

    trying new variables for math

--- script.js
+++ script.js
@@ -17,34 +17,46 @@ function writePassword() {
 }
 
 generatePassword = function() {
-   var passLength =  
-     window.prompt("Enter the length of your password between 8-128 characters.");
-    if (passLength < 8 || passLength > 128) {
-      window.alert("Please pick a number between 8 - 128");
-      return generatePassword();
-    }
+  var passLength =  window.prompt("Enter the length of your password between 8-128 characters.");
+  if (passLength < 8 || passLength > 128) {
+    window.alert("Please pick a number between 8 - 128");
+    return generatePassword();
+  }
   
-    else  { 
-      
-      var chooseCaps = window.confirm("Would you like upper case letters?");
+  else  {     
+    var chooseCaps = window.confirm("Would you like upper case letters?");
+        
+          if(chooseCaps) {
+             return upperLetters[Math.floor(Math.random() * upperLetters.length)]  
+          }
+          else {
+          var chooseLow = window.confirm("Would you like lower case letters?");
     
-      if(chooseCaps) {
-         return upperLetters[Math.floor(Math.random() * upperLetters.length)]  
-      }
-      else {
-      var chooseLow = window.confirm("Would you like lower case letters?");
-
-      if (chooseLow) {
-        return lowerLetters[Math.floor(Math.random() * lowerLetters.length)]
-      }
-    }
-    }
+          if (chooseLow) {
+            return lowerLetters[Math.floor(Math.random() * lowerLetters.length)]
+          }
+        }
+  }
   
 };
 
-// passNumbers = window.confirm("Would you like numbers?");       
-// lowerLetters = window.confirm("Would you like lower case letters?");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+// var setPassword = function() {
+
+//     var chooseCaps = window.confirm("Would you like upper case letters?");
+//     if (chooseCaps){
+//     return upperLetters[Math.floor(Math.random() * upperLetters.length)]
+//     }
+//     else {
+//       var chooseLow = window.confirm("Would you like lower case letters?");
+//       if (chooseLow) {
+//     return lowerLetters[Math.floor(Math.random() * lowerLetters.length)]
+//   }
+// }
+// }
+// passNumbers = window.confirm("Would you like numbers?");       
+// lowerLetters = window.confirm("Would you like lower case letters?");
