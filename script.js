929e33f0c84bb8a5bf8e6be8e3c1649ed60590e9
Ryan Murphy <TheDecoder007@gmail.com>
2022-05-12 12:45:18 -0400

    figured out loop

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Assignment code here
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";     
-var lowerLetters = "abcdefghijklmnopqrstuvwxyz";   
-var passNumbers = "1234567890";
+var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];     
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];   
+var passNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0,];
 
 // Write password to the #password input
 function writePassword() {
@@ -13,72 +11,123 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+};
 
-}
 
-generatePassword = function() {
-  var passLength =  window.prompt("Enter the length of your password between 8-12 characters.");
-      if (passLength < 8 || passLength > 12) {
-      window.alert("Please pick a number between 8 - 12");
-      return generatePassword();
-  }
-      if (passLength >= 8 || passLength <= 12)  {
-        console.log(passLength);
-      var chooseCaps = window.confirm("Would you like to use upper case letters?");
-      var chooseLow = window.confirm("Would you like use lower case letters?");
-      var chooseNum = window.confirm("Would you like to use numbers?");
+  var passConfirm = (passLength) => {
+    let passArray = [];
+  
+    var chooseCaps = window.confirm("Would you like to use upper case letters?");
+    var chooseLow = window.confirm("Would you like use lower case letters?");
+    var chooseNum = window.confirm("Would you like to use numbers?");
 
-            if(chooseCaps === true && chooseLow === true && chooseNum === true) {
-               return upperLetters[Math.floor(Math.random() * upperLetters.length)] +
-                lowerLetters[Math.floor(Math.random() * lowerLetters.length)] + 
-                passNumbers[Math.floor(Math.random() * passNumbers.length)]
+      let randomPassArray = [];
+
+          if(chooseCaps === true && chooseLow === true && chooseNum === true) {
+            randomPassArray = randomPassArray.concat(upperLetters, lowerLetters, passNumbers);
             }    
         else if(chooseCaps === false && chooseLow === true && chooseNum === true) {
-              return lowerLetters[Math.floor(Math.random() * lowerLetters.length)] +
-              passNumbers[Math.floor(Math.random() * passNumbers.length)] 
+          randomPassArray = randomPassArray.concat(lowerLetters, passNumbers);        
             }
         else if(chooseCaps === true && chooseLow === false && chooseNum == true) {
-              return upperLetters[Math.floor(Math.random() * upperLetters.length)] +
-              passNumbers[Math.floor(Math.random() * passNumbers.length)] 
-            }
+          randomPassArray = randomPassArray.concat(upperLetters, passNumbers);
+        }
         else if(chooseCaps === true && chooseLow === true && chooseNum == false) {
-              return upperLetters[Math.floor(Math.random() * upperLetters.length)] +
-              lowerLetters[Math.floor(Math.random() * lowerLetters.length)] 
-            }
+          randomPassArray = randomPassArray.concat(upperLetters, lowerLetters);
+        }
         else if(chooseCaps === false && chooseLow === false && chooseNum == true) {
-              return passNumbers[Math.floor(Math.random() * passNumbers.length)] 
-            } 
+          randomPassArray = randomPassArray.concat(passNumbers);
+        }
         else if(chooseCaps === false && chooseLow === true && chooseNum == false) {
-              return lowerLetters[Math.floor(Math.random() * lowerLetters.length)] 
-            }
+          randomPassArray = randomPassArray.concat(lowerLetters);          
+        }
         else if(chooseCaps === true && chooseLow === false && chooseNum == false) {
-            return upperLetters[Math.floor(Math.random() * upperLetters.length)] 
-            }
+          randomPassArray = randomPassArray.concat(upperLetters);
+        }
         else if (chooseCaps === false && chooseLow === false && chooseNum === false) {
               window.alert("You need to choose at lease 1 criteria")
               return generatePassword();
             }
-    }  
+        for (var i = 0; i <passLength; i++) {
+          let randomCharacter = Math.floor(Math.random() * randomPassArray.length);
+              passArray.push(randomPassArray[randomCharacter])
+        }  
+        return passArray.join("");
+       
+}
+
+var generatePassword = () => {
+  var passLength = window.prompt("Please choose your password length, between 8 and 20 characters.")
+    if (LengthChoice(passLength)) {
+      return (passConfirm(passLength))
+    } else {}
+}
+
+var LengthChoice = (userChoice) => {
+  if (userChoice === null){
+    window.alert("Please choose a length between 8 and 20")
+    return false
+  } else if (userChoice < 8 || userChoice > 20) {
+    window.alert("Please choose a length between 8 and 20")
+    return false
+  }
+    return true
 };
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-// var setPassword = function() {
 
-//     var chooseCaps = window.confirm("Would you like upper case letters?");
-//     if (chooseCaps){
-//     return upperLetters[Math.floor(Math.random() * upperLetters.length)]
-//     }
-//     else {
-//       var chooseLow = window.confirm("Would you like lower case letters?");
-//       if (chooseLow) {
-//     return lowerLetters[Math.floor(Math.random() * lowerLetters.length)]
+
+
+
+// generatePassword = function() {
+//   var passLength =  window.prompt("Enter the length of your password between 8-12 characters.");
+//       if (passLength < 8 || passLength > 12) {
+//       window.alert("Please pick a number between 8 - 12");
+//       return generatePassword();
 //   }
+//       if (passLength >= 8 || passLength <= 12)  {
+//         console.log(passLength);
+//       var chooseCaps = window.confirm("Would you like to use upper case letters?");
+//       var chooseLow = window.confirm("Would you like use lower case letters?");
+//       var chooseNum = window.confirm("Would you like to use numbers?");
+
+// if(chooseCaps === true && chooseLow === true && chooseNum === true) {
+//   return upperLetters[Math.floor(Math.random() * upperLetters.length)] +
+//    lowerLetters[Math.floor(Math.random() * lowerLetters.length)] + 
+//    passNumbers[Math.floor(Math.random() * passNumbers.length)]
+// }    
+// else if(chooseCaps === false && chooseLow === true && chooseNum === true) {
+//  return lowerLetters[Math.floor(Math.random() * lowerLetters.length)] +
+//  passNumbers[Math.floor(Math.random() * passNumbers.length)] 
 // }
+// else if(chooseCaps === true && chooseLow === false && chooseNum == true) {
+//  return upperLetters[Math.floor(Math.random() * upperLetters.length)] +
+//  passNumbers[Math.floor(Math.random() * passNumbers.length)] 
 // }
-// passNumbers = window.confirm("Would you like numbers?");       
-// lowerLetters = window.confirm("Would you like lower case letters?");
+// else if(chooseCaps === true && chooseLow === true && chooseNum == false) {
+//  return upperLetters[Math.floor(Math.random() * upperLetters.length)] +
+//  lowerLetters[Math.floor(Math.random() * lowerLetters.length)] 
+// }
+// else if(chooseCaps === false && chooseLow === false && chooseNum == true) {
+//  return passNumbers[Math.floor(Math.random() * passNumbers.length)] 
+// } 
+// else if(chooseCaps === false && chooseLow === true && chooseNum == false) {
+//  return lowerLetters[Math.floor(Math.random() * lowerLetters.length)] 
+// }
+// else if(chooseCaps === true && chooseLow === false && chooseNum == false) {
+// return upperLetters[Math.floor(Math.random() * upperLetters.length)] 
+// }
+// else if (chooseCaps === false && chooseLow === false && chooseNum === false) {
+//  window.alert("You need to choose at lease 1 criteria")
+//  return generatePassword();
+// }
+// for (var i = 0; i <passLength; i++) {
+// let randomCharacter = Math.floor(Math.random() * randomPassArray.length);
+//  passArray.push(randomPassArray[randomCharacter])
+// }  
+// return passArray.join("");
+
+// }
